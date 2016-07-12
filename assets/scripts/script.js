cf719ac2c196ae08badc24b517eed10771f84c99
JeremyGralla <jeremy.gralla@mail.utoronto.ca>
2016-07-12 01:32:02 -0400

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
     $("button.main").click(function() {
-        $("article").addClass("hidden");
+        $("article#profile").toggleClass("hidden");
+        $("article#main").toggleClass("hidden");
+    });
+    $("button.profile").click(function() {
+        $("article#profile").toggleClass("hidden");
+        $("article#main").toggleClass("hidden");
+    });
+    $("button.about").click(function() {
+        $("article#about").toggleClass("hidden");
+        $("article#main").toggleClass("hidden");
+    });
+    $("button.aboutback").click(function() {
+        $("article#about").toggleClass("hidden");
+        $("article#main").toggleClass("hidden");
     });
 });
