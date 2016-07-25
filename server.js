64d91ee3afca41fd43415b43b516a522bf8f2a96
Salman Fazal <salman.fazal@mail.utoronto.ca>
2016-07-25 15:33:20 -0400

    Add files via upload
    It's now recieving JSON data from the frontend upon submission. Now working on getting the format right

--- server.js
+++ server.js
@@ -6,26 +6,48 @@ const PORT = 3000; //declare your listening port
 http.createServer(function(request, response){
 	console.log(request.url);
 	
-	//var file_Name = request.url.toString(); 
-	if(request.url == "/"){
+	//var file_Name = request.url.toString();
+	if (request.method == 'POST') {
+		var body = '';
+		request.on('data', function(data){
+			body += data;
+		})
+		request.on('end', function(){
+			if(request.url == "/carcoolersignup"){
+				//coolersingup(body, );
+				console.log(JSON.parse(body));
+			}else if(request.url == "/carrulersignup"){
+				//rulersignup(request.data)
+				console.log(body);
+			}
+		})
+		
+
+	}else{
+
+		if(request.url == "/"){
 		
 		sendRes(response, "index.html", "text/html");
 
 		
-	}else if(request.url == "/assets/scripts/script.js"){
-		sendRes(response, "assets/scripts/script.js", "text/javascript");
-	}else if(request.url == "/assets/scripts/jquery-2.2.4.min.js"){
-		sendRes(response, "assets/scripts/jquery-2.2.4.min.js", "text/javascript");
-	}else if(request.url == "/assets/css/style.css"){
-		sendRes(response, "assets/css/style.css", "text/css");
-	/*}else{
-		//call function to decide what JSON data to send
-		sendJson(request.url, response);
-		//sendRes(response, 'nytimes.json', "text/json" );
-		//response.write('nytimes.json');
-	*/
-		
+		}else if(request.url == "/assets/scripts/script.js"){
+			sendRes(response, "assets/scripts/script.js", "text/javascript");
+		}else if(request.url == "/assets/scripts/jquery-2.2.4.min.js"){
+			sendRes(response, "assets/scripts/jquery-2.2.4.min.js", "text/javascript");
+		}else if(request.url == "/assets/css/style.css"){
+			sendRes(response, "assets/css/style.css", "text/css");
+
+		}
+		/*}else{
+			//call function to decide what JSON data to send
+			sendJson(request.url, response);
+			//sendRes(response, 'nytimes.json', "text/json" );
+			//response.write('nytimes.json');
+		*/
+			
 	}
+
+	
 
 }).listen(PORT, function()
 
